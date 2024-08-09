4db60c8251b66fc47e9eb2ef2e4f5024b003beba
ombhombe2344 <ombhombe15@gmail.com>
2024-08-09 11:22:14 +0530

    Create printarray.cpp

---
+++ printarray.cpp
@@ -0,0 +1,25 @@
+//om bhombe
+//23070123039
+#include <iostream>
+using namespace std;
+int main() {
+ 
+ int x[7] = {19,14,9,34,87,76};
+ for(int i = 0;i<7;i++)
+ {cout << x[i];
+    cout<< endl;
+ }
+
+
+    return 0;
+}
+
+
+/*19
+14
+9
+34
+87
+76
+0
+*/
